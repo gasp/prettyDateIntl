b209d40b3fdfe4046f794b2b7d7aa32ac3180d39
gaspard <gaspard@gmail.com>
2012-05-15 16:14:14 +0200

    rearranging
    putting init() at end of script

--- jq.prettyDateIntl.js
+++ jq.prettyDateIntl.js
@@ -1,15 +1,31 @@
 (function( $ ){
 	var methods = {
-		init : function(){},
-		set : function(){}
-	}
-	$.fn.dateme = function( options ) {  
- 
-		var settings = $.extend( {
-			'lang'         : 'en',
-		}, options);
+//		lang : 'en',
+		change : function(){
+			
+		},
 
-/* 
+		makedate : function(){
+			return 'trolo'+text;
+		},
+		init : function(options){
+// may extend ?
+
+
+			//console.log(this);
+			
+			return this.each(function() { 
+				console.log(makedate());
+				
+				var t = (this.title)? this.title:$(this).html();
+				var date = 'trolo'+t;//makedate(t);
+				if ( date )
+					jQuery(this).text( date );
+			});
+		}
+	}
+	$.fn.dateme = function( method ) {  
+
 		if ( methods[method] ) {
 			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
 		} else if ( typeof method === 'object' || ! method ) {
@@ -17,16 +33,6 @@
 		} else {
 			$.error( 'Method ' +  method + ' does not exist on the plugin' );
 		}
- */
-		return this.each(function() {        
- 
-			var t = (this.title)? this.title:$(this).html();
-			
-			var date = prettyDate(t);
-				if ( date )
-					jQuery(this).text( date );
- 
-		});
  
 		function prettyDate(text){
 			return 'trolo'+text;
